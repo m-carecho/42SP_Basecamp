f977f79d3b96796086f983c753669a7f29492a32
m-carecho <milena.c@aluno.ifsp.edu.br>
2022-02-12 23:23:08 -0300

    ex C00 - 00, 01, 02

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarecho <mcarecho@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/12 17:26:32 by mcarecho          #+#    #+#             */
+/*   Updated: 2022/02/12 17:26:32 by mcarecho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		write(1, &alpha, 1);
+		alpha++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+}
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarecho <mcarecho@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/12 17:26:35 by mcarecho          #+#    #+#             */
+/*   Updated: 2022/02/12 17:26:35 by mcarecho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'z';
+	while (alpha >= 'a')
+	{
+		write(1, &alpha, 1);
+		alpha--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+}
